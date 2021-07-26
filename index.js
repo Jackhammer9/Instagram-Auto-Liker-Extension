ea475ed2cb62b45de105f4df034df4607cff13dc
Arnav Bajaj <arnavbajaj9@gmail.com>
2021-07-26 18:32:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,68 @@
+let isRunning = false
+const runBtn = document.getElementById("run-btn")
+const stopBtn = document.getElementById("stop-btn")
+let postsToLike = 0
+let timeBetweenLikes = 3;
+const timeBetweenLikeEl = document.getElementById("input-el")
+const toLikeEL = document.getElementById("posts-el")
+let postsLiked = 0
+const counterEl = document.getElementById("liked-el")
+
+stopBtn.disabled =true
+
+runBtn.addEventListener("click" , () => {
+    isRunning = true
+    stopBtn.disabled =false
+    runBtn.disabled=true
+    timeBetweenLike = parseInt(timeBetweenLikeEl.value)
+    postsToLike = parseInt(toLikeEL.value)
+})
+
+stopBtn.addEventListener("click" , () => {
+    isRunning = false
+    postsLiked = 0
+    stopBtn.disabled =true
+    runBtn.disabled=false
+})
+
+const likePost = () => {
+    const likeBtn = document.getElementsByClassName("wpO6b  ")
+    const arrowBtn = document.querySelector(".coreSpriteRightPaginationArrow")
+    likeBtn[1].click()
+    arrowBtn.click()
+}
+
+const updateLikedCounter = () =>{
+    postsLiked += 1
+    counterEl.textContent = "Posts Liked: "+JSON.stringify(postsLiked)
+}
+
+setInterval(() => {
+    if (postsToLike > 0){
+        if (isRunning && postsLiked < postsToLike){
+            updateLikedCounter()
+            chrome.tabs.query({"active" : true} , (tabs) => {
+                let tab = tabs[0]
+                chrome.scripting.executeScript({
+                    target: {tabId: tab.id},
+                    function: likePost
+                })
+            })
+        }
+        else if (postsLiked >= postsToLike){
+            stopBtn.click()
+        }
+    }
+    else{
+        if (isRunning){
+            updateLikedCounter()
+            chrome.tabs.query({"active" : true} , (tabs) => {
+                let tab = tabs[0]
+                chrome.scripting.executeScript({
+                    target: {tabId: tab.id},
+                    function: likePost
+                })
+            })
+        }
+    }
+},timeBetweenLikes*1000)
